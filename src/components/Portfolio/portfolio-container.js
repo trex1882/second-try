416efb031c342e2e023c655c82ca552252e5fd21
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-07-06 21:04:03 -0600

    Commit

--- src/components/Portfolio/portfolio-container.js
+++ src/components/Portfolio/portfolio-container.js
@@ -8,6 +8,7 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: "Welcome to my portfolio",
+            isLoding: false,
             data: [
                 { title: "Quip", category: "eCommerce" },
                 { title: "EventBright", category: "Scheuduling" },
@@ -35,6 +36,9 @@ export default class PortfolioContainer extends Component {
 
 
     render() {
+        if (this.state.isLoding) {
+            return <div>Loading...</div>;
+        }
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 import PortfolioContainer from "./Portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container.js"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         
+        <Router>
+          <div>
+            
+          </div>
+        </Router>
+
         <h1>number two page</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
         <PortfolioContainer />
