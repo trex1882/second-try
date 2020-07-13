8e60843cd26f231526547ddb7b21752eff89290c
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-07-13 15:08:52 -0600

    commit

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,7 +1,28 @@
-  
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log("Handle submit", event);
+  }
+
   render() {
     return (
       <div>
@@ -9,24 +30,24 @@ export default class Login extends Component {
 
         <form onSubmit={this.handleSubmit}>
           <input
-           type="email"
-           name="email"
-           placeholder="Your email"
-           value={this.state.email}
-           onChange={this.handleSubmit}
-            />
+            type="email"
+            name="email"
+            placeholder="Your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
 
-            <input
-           type="password"
-           name="password"
-           placeholder="Your password"
-           value={this.state.password}
-           onChange={this.handleSubmit}
-            />
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
 
-            <div>
-              <button type="submit">Log In</button>
-            </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
         </form>
       </div>
     );
