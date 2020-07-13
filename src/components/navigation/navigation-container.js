1ca6a4228934a19b57e903f4f8560eb50bcad537
Jessica Colby <jessicacolby@Jessicas-MacBook-Air.local>
2020-07-13 12:11:18 -0600

    comit

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,7 +8,9 @@ export default class NavigationContainer extends Component {
 
     render() {
         return (
-            <div>
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <div className="nav-link-wrapper">
                 <NavLink exact to="/">Home</NavLink>
                 <NavLink to="/about-me">About</NavLink>
                 <NavLink to="/contact">Contact</NavLink>
